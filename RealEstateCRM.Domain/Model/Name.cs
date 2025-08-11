959886c70a732a01aced7b50bfe72f64d7bfc0a1
ALampreia <andre.f.s.lampreia@gmail.com>
2025-08-11 15:44:59 +0100

    Added name class

--- RealEstateCRM.Domain/Model/Name.cs
+++ RealEstateCRM.Domain/Model/Name.cs
@@ -8,9 +8,34 @@ namespace RealEstateCRM.Domain.Model
 {
     internal class Name
     {
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
+        public string FirstName { get; private set; }
+        public string MiddleNames { get; private set; }
+        public string LastName { get; private set; }
 
+        public string FullName => $"{FirstName} {string.Join(" ", MiddleNames)} {LastName}";
+
+        private Name()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            MiddleNames = string.Empty;
+        }
+
+        public Name(string firstName, string lastName) : this()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Name(string firstName, string middleNames, string lastName) : this(firstName, lastName)
+        {
+            MiddleNames = middleNames;
+        }
+
+        public Name(string fullName) : this(
+              fullName.Split(" ")[0],
+              fullName.Split(" ")[fullName.Split(" ").Length - 1])
+        {
+        }
     }
 }
