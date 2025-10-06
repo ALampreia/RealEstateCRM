80f5abe9bcdb6047dfec2b7062bd602e63a6d6d2
ALampreia <andre.f.s.lampreia@gmail.com>
2025-10-06 16:25:43 +0100

    Dtos and Fluent

--- RealEstateCRM.Services/Properties/RegisterPropertyCommandValidator.cs
+++ RealEstateCRM.Services/Properties/RegisterPropertyCommandValidator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,11 @@ using System.Threading.Tasks;
 
 namespace RealEstateCRM.Services.Properties
 {
-    public class RegisterPropertyCommandValidator
+    public class RegisterPropertyCommandValidator : AbstractValidator<RegisterPropertyCommand>
     {
-
+        public RegisterPropertyCommandValidator()
+        {
+            RuleFor(x => x.PropertyDto.AddressDto.).NotEmpty();
+        }
     }
 }
